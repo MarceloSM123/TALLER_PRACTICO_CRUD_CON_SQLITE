0fb963cefe97aacd475538ee99976fa55b3baf82
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-09-22 01:16:25 -0500

    PASO 4: El Núcleo de la App (App.tsx)

--- CazadorDeSabores/App.tsx
+++ CazadorDeSabores/App.tsx
@@ -1,26 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 import { SQLiteProvider } from 'expo-sqlite';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
+import { useState, useEffect } from 'react';
+import { initDatabase } from './src/database/db';
+import ListaScreen from './src/screens/ListaScreen';
+import FormularioScreen from './src/screens/FormularioScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [dbLista, setDbLista] = useState(false);
+  useEffect(() => {
+    const prepDb = async () => {
+      await initDatabase();
+      setDbLista(true)
+    };
+    prepDb();
+  }, [])
+
+  if (!dbLista) {
+    return (
+      <View>
+        <ActivityIndicator size={'large'} color={'blue'} />
+        <Text> Cargando base de datos</Text>
+      </View>
+    )
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SQLiteProvider databaseName='BaseDatos.db'>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Lista'
+            component={ListaScreen}
+            options={{ title: 'Lista' }}
+          />
+          <Stack.Screen
+            name='Formulario'
+            component={FormularioScreen}
+            options={{ title: 'Formulario' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SQLiteProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
