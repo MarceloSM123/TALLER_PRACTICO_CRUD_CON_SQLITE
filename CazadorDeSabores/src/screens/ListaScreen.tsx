ff08cb0d7e84456ad05ba8ba67ba45f361b82d4f
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-09-22 20:19:54 -0500

    PASO 5: El Catálogo (ListaScreen.tsx)

--- CazadorDeSabores/src/screens/ListaScreen.tsx
+++ CazadorDeSabores/src/screens/ListaScreen.tsx
@@ -1,9 +1,69 @@
-import {View,Text} from 'react-native'
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useSQLiteContext } from 'expo-sqlite'
+import { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import FormularioScreen from './FormularioScreen'
 
-export default function ListaScreen(){
-    return(
+type Registro = {
+    id: number,
+    titulo: string,
+    calificacion: number,
+    comentarios: string,
+    fotoBase64: string,
+    fecha: string
+}
+
+export default function ListaScreen({ navigation }: any) {
+
+    const db = useSQLiteContext();
+    const [registro, setRegistro] = useState<Registro[]>([]);
+
+    const cargarRegistro = async () => {
+        const resultado = await db.getAllAsync<Registro>('SELECT * FROM registros ORDER BY id DESC');
+        setRegistro(resultado)
+    }
+
+    useEffect(() => {
+        const unsuscribe = navigation.addListener("focus", () => {
+            cargarRegistro();
+            return unsuscribe
+        })
+    }, [navigation])
+
+    const borrarRegistro = async (id: number) => {
+        await db.runAsync("DELETE FROM registros WHERE id=?", [id]);
+        cargarRegistro();
+    }
+    return (
         <View>
-            <Text> Lista Screen </Text>
+            <FlatList
+                data={registro}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+
+                    <View>
+                        <View>
+                            <Image
+                                source={{ uri: `data: image/jpeg;base64, ${item.fotoBase64}` }}
+                                style={{ width: 80, height: 80 }}
+                            />
+                        </View>
+                        <Text>{item.titulo}</Text>
+                        <Text>{item.calificacion}</Text>
+                        <Text>{item.comentarios}</Text>
+                        <TouchableOpacity onPress={() => borrarRegistro}>
+                            <Ionicons name='trash' size={24} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate('FormularioScreen')}>
+                            <Ionicons name='add' size={24} />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate('FormularioScreen')}>
+                            <Ionicons name='pencil' size={24} />
+                        </TouchableOpacity>
+
+                    </View>
+                )}
+            />
         </View>
     )
 }
